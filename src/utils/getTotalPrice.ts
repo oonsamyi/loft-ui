ca310b8654d53f52e375509714b9040077f94773
Oleg Utitsyn <o.uticyn@cian.ru>
2021-05-29 03:56:29 +0300

    Фикс расчета цены объявки

--- src/utils/getTotalPrice.ts
+++ src/utils/getTotalPrice.ts
@@ -2,7 +2,9 @@ import { differenceInHours } from 'date-fns'
 import { IDate } from '../types'
 
 export function getTotalPrice(price: number, date: IDate): number {
-  const hours = differenceInHours(date.to, date.from) || 1
+  const hours = Math.abs(differenceInHours(date.to, date.from)) || 1
+  const minutes = Math.abs(date.to.getMinutes() - date.from.getMinutes())
+  const additionalHour = minutes ? 1 : 0
 
-  return hours * price
+  return (hours + additionalHour) * price
 }
